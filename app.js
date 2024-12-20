9725c1a41101b08439cdbc799934142deea749aa
Garvis123 <parvmodi11@gmail.com>
2024-12-20 11:55:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express=require('express');
+const app=express();
+const socket=require('socket.io');
+const http=require('http');
+const {Chess}=require("chess.js"); 
+const path = require('path');
+const { log } = require('console');
+// const js=require("./public/js");
+const server=http.createServer(app);
+const io=socket(server);
+
+const chess=new Chess();
+
+let players={};
+let currentPlayer="w";
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection",function(uniquesocket)
+{
+    console.log("connected");
+    if(!players.white)
+    {
+        players.white=uniquesocket.id;
+        uniquesocket.emit("playerRole","w")
+    }
+    else if(!players.black)
+    {
+        players.black=uniquesocket.id;
+        uniquesocket.emit("playerRole","b");
+    }
+    else 
+    {
+        uniquesocket.emit("spectatorRole");
+    }
+    uniquesocket.on("disconnect",function()
+{
+    if(uniquesocket.id===players.white)
+    {delete players.white;}
+    else if(uniquesocket.id===players.black)
+    {delete players.black;}
+})
+
+uniquesocket.on("move",function(move)
+{ try{
+
+    if(chess.turn() ==="w" && uniquesocket.id !== players.white) return;
+    if(chess.turn() ==="b" && uniquesocket.id !== players.black ) return;
+    const result=chess.move(move);
+    if(result){
+        currentPlayer=chess.turn();
+        io.emit("move",move);
+        io.emit("boardState",chess.fen())
+    }
+    else{
+        alert("Invalid move :",move);
+        uniquesocket.emit("inavalidMove",move);
+    }
+}
+catch(err)
+{
+    uniquesocket.emit("Inavalid move: ",move);
+ 
+}
+});
+});
+
+
+app.get("/",function(req,res)
+{
+    res.render("index",{title:"Chess Game"});
+})
+
+
+server.listen(3000);
